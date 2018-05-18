d5bc9ba4154e210ff2d62c7259006db4bd780c95
Yura Kruhlyk <murove1q@gmail.com>
2018-05-18 12:21:02 +0300

    init

--- app/modules/auth/selectors.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from 'reselect';
-import R from 'ramda';
-
-export const getSigningUpState = createSelector(
-  R.pathOr(0, ['auth', 'isSigningUp']),
-  state => state,
-);
--- app/modules/auth/types.js
+++
@@ -1,3 +0,0 @@
-export const SIGN_UP_START = 'auth/SIGN_UP_START';
-export const SIGN_UP_SUCCESS = 'auth/SIGN_UP_SUCCESS';
-export const SIGN_UP_ERROR = 'auth/SIGN_UP_ERROR';
